8257474e0f663f3a625ae3c9f26a2827e56e2d28
Joao-Pedro-B <joaobarbalho19@gmail.com>
2025-11-18 00:30:11 -0300

    update

--- pag03 (vocacional)/ec/testevoc-result/script.js
+++ pag03 (vocacional)/ec/testevoc-result/script.js
@@ -1,6 +1,6 @@
 document.getElementById("botaoResultado").addEventListener("click", function() {
 
-  const pontos = { medicina: 0, adm: 0, sistemas: 0, vet: 0 };
+  const pontos = { medicina: 0, adm: 0, sistemas: 0, vet: 0, direito: 0};
 
   const perguntas = document.querySelectorAll(".pergunta");
 
@@ -17,19 +17,30 @@ document.getElementById("botaoResultado").addEventListener("click", function() {
     pontos[curso] += Number(resposta.value);
   }
 
-  const maior = Math.max(...Object.values(pontos));
+  const valores = Object.values(pontos);
+  const maior = Math.max(...valores);
+
+  //  VERIFICAÇÃO DE EMPATE TOTAL
+  const empateTotal = valores.every(v => v === valores[0]);
+  if (empateTotal) {
+    document.getElementById("resultado").textContent =
+      "Os resultados ficaram iguais para todos os cursos. Por favor, refaça o teste para uma análise mais precisa!";
+    return;
+  }
+  //FIM DO CÓDIGO ADICIONADO
 
   const melhores = Object.keys(pontos).filter(curso => pontos[curso] === maior);
 
   let mensagem = "";
 
-  if (melhores.length === 1) {
+  if (melhores.length = 1) {
     const curso = melhores[0];
     const nomes = {
       medicina: "Medicina",
       adm: "Administração",
       sistemas: "Sistemas de Informação",
-      vet: "Veterinária"
+      vet: "Veterinária",
+      direito: "direito"
     };
     mensagem = `Você combina mais com ${nomes[curso]}!`;
   } else {
@@ -37,14 +48,14 @@ document.getElementById("botaoResultado").addEventListener("click", function() {
       medicina: "Medicina",
       adm: "Administração",
       sistemas: "Sistemas de Informação",
-      vet: "Veterinária"
+      vet: "Veterinária",
+      direito: "direito"
     }[c]));
     mensagem = `Seu perfil está equilibrado entre: ${nomesBonitos.join(" e ")}!`;
   }
 
   document.getElementById("resultado").textContent = mensagem;
 
- 
   document.getElementById('mensagemFinal').style.display = 'block';
 
 });
